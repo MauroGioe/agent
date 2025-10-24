857d6aa00d61f4eca4df5a223a992c87003a2657
mauro <maurogioe91@libero.it>
2025-10-24 15:43:49 +0200

    removed bat file

--- execute.bat
+++
@@ -1,6 +0,0 @@
-C:\Users\mauro\PycharmProjects\a2a_agent\.venv\Scripts\python.exe exchange_server.py &
-C:\Users\mauro\PycharmProjects\a2a_agent\.venv\Scripts\python.exe math_server.py &
-C:\Users\mauro\PycharmProjects\a2a_agent\.venv\Scripts\python.exe math_agent_server.py &
-C:\Users\mauro\PycharmProjects\a2a_agent\.venv\Scripts\python.exe exchange_agent_server.py &
-C:\Users\mauro\PycharmProjects\a2a_agent\.venv\Scripts\python.exe run_from_local_client.py &
-wait
